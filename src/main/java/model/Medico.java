2a7bc28fb3c6312181f8f83aef02c9e89a4433fd
corr-iov1 <corr.iovino@studenti.unina.it>
2026-05-01 17:37:31 +0200

    Update Medico.java

--- src/main/java/model/Medico.java
+++ src/main/java/model/Medico.java
@@ -6,45 +6,52 @@ public class Medico extends Utente {
     private String nome;
     private String cognome;
     private Reparto reparto
-private ArrayList<Prestazione> prestazioni = new ArrayList<>();
-private ArrayList<TurnoLavorativo> turni = new ArrayList<>();
-
-
+    private ArrayList<Prestazione> prestazioni = new ArrayList<>();
+    private ArrayList<TurnoLavorativo> turni = new ArrayList<>();
 
     public Medico(String login, String password, String nome, String cognome, Reparto reparto) {
         super(login, password);
         this.nome = nome;
         this.cognome = cognome;
         this.reparto = reparto;
-        }
+    }
 
-public String getNome(){
+    public String getNome() {
         return this.nome;
     }
-public void setNome(String nome){
+
+    public void setNome(String nome) {
         this.nome = nome;
-}
+    }
 
-public String getCognome(){
+    public String getCognome() {
         return this.cognome;
-}
+    }
 
-public void setCognome(String cognome){
+    public void setCognome(String cognome) {
         this.cognome = cognome;
-}
+    }
 
-public Reparto getReparto(){
-return reparto;
-}
+    public Reparto getReparto() {
+        return reparto;
+    }
 
-public void setReparto(Reparto r){
+    public void setReparto(Reparto r) {
         this.reparto = r;
 
-}
-public void registerEsito(Prestazione p, String testo){
+    }
+
+    public ArrayList<TurnoLavorativo> getTurni() {
+        return turni;
+    }
+
+    public ArrayList<Prestazione> getPrestazioni() {
+        return prestazioni;
+    }
+
+    public void registerEsito(Prestazione p, String testo) {
         p.setEsito(testo);
+
+    }
+
 }
-
-
-}
-
